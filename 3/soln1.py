d0ee37f6eaece7140da81e86e2f4a06078ae1272
Marcus Fedarko <mfedarko@ucsd.edu>
2023-12-04 03:04:47 -0800

    Tidy up 3-1

--- 3/soln1.py
+++ 3/soln1.py
@@ -31,15 +31,14 @@ for li, line in enumerate(lines):
                         if oc not in DIGITS and oc != ".":
                             isp = True
                             break
-                if curr_num_start > 0 and line[left] not in DIGITS and line[left] != ".":
-                    isp = True
-
-                if ci < len(line) - 1 and line[right] not in DIGITS and line[right] != ".":
-                    isp = True
-
+                if not isp:
+                    # check the characters to the left and right of the number
+                    # (no need to check that the characters are not in DIGITS,
+                    # we already know that they aren't -- if they were, then
+                    # this would be a different number ;)
+                    isp = (curr_num_start > 0 and line[left] != ".") or (ci < len(line) - 1 and line[right] != ".")
                 if isp:
                     pns += int(curr_num)
-                print(curr_num, curr_num_start, left, right, isp)
                 curr_num = None
                 curr_num_start = None
 print(pns)
